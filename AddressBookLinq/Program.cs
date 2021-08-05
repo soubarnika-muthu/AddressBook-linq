ff34ed05082ac38ae6634ed817a5a4dc8b3a79f8
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-05 22:53:28 +0530

    [Soubarnika]Executing AddressBook using linq

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing AddressBook using linq");
+        }
+    }
+}
